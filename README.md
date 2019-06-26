0c0f582c6ac5c31ad04735a983ccf598496d80f5
Himenon <6715229+Himenon@users.noreply.github.com>
2019-06-26 23:48:35 +0900

    initialize

---
+++ README.md
@@ -0,0 +1,30 @@
+# 怠惰な設定のフロントエンド開発環境
+
+## 開発を始める
+
+```bash
+yarn
+yarn start
+```
+
+## ビルドする
+
+```bash
+yarn run build
+```
+
+ビルドした成果物をサーバーで確認する
+
+```bash
+yarn run serve
+```
+
+## format
+
+```bash
+yarn run format
+```
+
+## License
+
+MIT
---
+++ webpack.config.ts
@@ -0,0 +1,41 @@
+import * as webpack from "webpack";
+import * as path from "path";
+import * as HtmlWebpackPlugin from "html-webpack-plugin";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+
+const generateConfig = (isProduction: boolean): webpack.Configuration => {
+  return {
+    mode: isProduction ? "production" : "development",
+    target: "web",
+    optimization: {
+      minimize: isProduction
+    },
+    entry: {
+      app: "./src/index.ts",
+      print: "./src/print.ts"
+    },
+    devtool: "inline-source-map",
+    devServer: {
+      contentBase: "./dist"
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        title: isProduction ? "Production" : "Development",
+        template: "public/index.html"
+      })
+    ],
+    output: {
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist")
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"]
+    },
+    module: {
+      rules: [{ test: /\.tsx?$/, loader: "ts-loader" }]
+    }
+  };
+};
+
+export default generateConfig(process.env.NODE_ENV === "production");
